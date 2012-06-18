f14d0552f6d2967c987fe265ac1c98e128672425
Nikolas Engelhard <lengelhan@atcremers21.informatik.tu-muenchen.de>
2012-06-18 15:33:16 +0200

    first checkin (result from roscreate-qt-pkg)

---
+++ include/projector_calibration/main_window.hpp
@@ -0,0 +1,64 @@
+/**
+ * @file /include/projector_calibration/main_window.hpp
+ *
+ * @brief Qt based gui for projector_calibration.
+ *
+ * @date November 2010
+ **/
+#ifndef projector_calibration_MAIN_WINDOW_H
+#define projector_calibration_MAIN_WINDOW_H
+
+/*****************************************************************************
+** Includes
+*****************************************************************************/
+
+#include <QtGui/QMainWindow>
+#include "ui_main_window.h"
+#include "qnode.hpp"
+
+/*****************************************************************************
+** Namespace
+*****************************************************************************/
+
+namespace projector_calibration {
+
+/*****************************************************************************
+** Interface [MainWindow]
+*****************************************************************************/
+/**
+ * @brief Qt central, all operations relating to the view part here.
+ */
+class MainWindow : public QMainWindow {
+Q_OBJECT
+
+public:
+	MainWindow(int argc, char** argv, QWidget *parent = 0);
+	~MainWindow();
+
+	void ReadSettings(); // Load up qt program settings at startup
+	void WriteSettings(); // Save qt program settings when closing
+
+	void closeEvent(QCloseEvent *event); // Overloaded function
+	void showNoMasterMessage();
+
+public slots:
+	/******************************************
+	** Auto-connections (connectSlotsByName())
+	*******************************************/
+	void on_actionAbout_triggered();
+	void on_button_connect_clicked(bool check );
+	void on_checkbox_use_environment_stateChanged(int state);
+
+    /******************************************
+    ** Manual connections
+    *******************************************/
+    void updateLoggingView(); // no idea why this can't connect automatically
+
+private:
+	Ui::MainWindowDesign ui;
+	QNode qnode;
+};
+
+}  // namespace projector_calibration
+
+#endif // projector_calibration_MAIN_WINDOW_H
